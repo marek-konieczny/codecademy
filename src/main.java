c61ba8808ceb268472a0feb25247b29c6b1c2c13
marek-konieczny <marek.konieczny1@gmail.com>
2015-11-05 15:17:31 +0100

    close10

--- src/main.java
+++ src/main.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.sound.midi.MetaEventListener;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 import javax.xml.stream.events.Comment;
 
@@ -194,11 +195,29 @@ public int intMax(int a, int b, int c) {
 			currMax=c;
 			return currMax;
 		}
-	}
-	//return currMax;
-	  
+	}	  
 }
+//--------------------------------------------------------------------------
+	//Given 2 int values, return whichever value is nearest to the value 10,
+	//or return 0 in the event of a tie. 
+	//Note that Math.abs(n) returns the absolute value of a number. 
 
+//close10(8, 13)  8
+//close10(13, 8)  8
+//close10(13, 7)  0
+public int close10(int a, int b) {
+	if(Math.abs(a-10)==Math.abs(b-10))
+		return 0;
+	else
+	{
+		if(Math.abs(a-10)>Math.abs(b-10))
+		return b;
+		else {
+			return a;	
+		}
+	}
+	//return (Math.min(Math.abs(a-10),Math.abs(b-10)));  
+}
 	public static void main(String[] args) throws ClassNotFoundException,
 			SQLException, IOException {
 		
