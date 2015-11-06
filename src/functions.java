2068b33224153ab1f6d8b257f65bc7afc3d2c1e0
marek-konieczny <marek.konieczny1@gmail.com>
2015-11-06 15:30:31 +0100

    repating string

--- src/functions.java
+++ src/functions.java
@@ -347,7 +347,7 @@ public class functions {
 				result = str.substring(0, 1);
 				str = str.substring(1);
 				while (str.length() >= n) {
-					str = str.substring(n-1);
+					str = str.substring(n - 1);
 					System.out.println(str);
 					result += str.substring(0, 1);
 					System.out.println(result);
@@ -358,4 +358,20 @@ public class functions {
 		}
 	}
 
+	// ----------------------------------------------------------------------------------------
+	// Given a string and a non-negative int n,
+	// return a larger string that is n copies of the original string.
+
+	// stringTimes("Hi", 2) "HiHi"
+	// stringTimes("Hi", 3) "HiHiHi"
+	// stringTimes("Hi", 1) "Hi"
+	public String stringTimes(String str, int n) {
+		String strTemp="";
+		  for(int i=0;i<n;i++)
+		  {
+			  
+			  strTemp+=str;
+		  }
+		  return str;
+	}
 }
