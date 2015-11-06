4090206c0dea0214c15a70fcf614fef92e1adcb2
marek-konieczny <marek.konieczny1@gmail.com>
2015-11-06 15:48:55 +0100

    wamr up 2

--- src/functions.java
+++ src/functions.java
@@ -366,12 +366,20 @@ public class functions {
 	// stringTimes("Hi", 3) "HiHiHi"
 	// stringTimes("Hi", 1) "Hi"
 	public String stringTimes(String str, int n) {
-		String strTemp="";
-		  for(int i=0;i<n;i++)
-		  {
-			  
-			  strTemp+=str;
-		  }
-		  return str;
+		String strTemp = "";
+		for (int i = 0; i < n; i++) {
+			strTemp += str;
+		}
+		return strTemp;
 	}
+
+	// -----------------------------------------------------------------------------------------
+	// Given a string and a non-negative int n,
+	// we'll say that the front of the string is the first 3 chars,
+	// or whatever is there if the string is less than length 3. Return n copies
+	// of the front;
+
+	// frontTimes("Chocolate", 2) "ChoCho"
+	// frontTimes("Chocolate", 3) "ChoChoCho"
+	// frontTimes("Abc", 3) "AbcAbcAbc"
 }
